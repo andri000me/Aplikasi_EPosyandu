f6dc46c730c1cb172b6c1e9fbf7c4bf3787feb98
Meilio Andree <meilioAndree27@gmail.com>
2021-04-07 10:49:31 +0700

    Update CRUD Anak dan Ibu

--- prosesCrudAnak.php
+++ prosesCrudAnak.php
@@ -1,30 +1,30 @@
 <?php
     include 'connection.php';
 
-    $op = $_GET['op'];
+    $func_anak = $_GET['func_anak'];
 
-    if($op == "ambiloption"){
-        $option = mysqli_query($conn, "SELECT nama_anak FROM ref_anak");
-        echo "<option>Pilih Nama Anak</option>\n";
+    if($func_anak == "ambil_option_anak"){
+        $option = mysqli_query($conn, "SELECT nik_anak FROM ref_anak");
+        echo "<option>Pilih NIK Anak</option>\n";
         
-        while($op = mysqli_fetch_array($option)){
-            echo "<option>".$op['nama_anak']."</option>\n";
+        while($func_anak = mysqli_fetch_array($option)){
+            echo "<option>".$func_anak['nik_anak']."</option>\n";
         }
     
-    } else if($op == "ambildata"){
-        $nama_anak = $_GET['nama_anak'];
-        $data = mysqli_query($conn, "SELECT * FROM ref_anak WHERE nama_anak='$nama_anak'");
+    } else if($func_anak == "ambil_data_anak"){
+        $nik_anak = $_GET['option_nik_anak'];
+        $data = mysqli_query($conn, "SELECT * FROM ref_anak WHERE nik_anak='$nik_anak'");
         $d = mysqli_fetch_array($data);
-        echo $d['nik_anak']."|".$d['tempat_lahir_anak']."|".$d['tanggal_lahir_anak']. "|".$d['usia']. "|". $d['jk_anak'];
+        echo $d['nama_anak']."|".$d['tempat_lahir_anak']."|".$d['tanggal_lahir_anak']. "|".$d['usia']. "|". $d['jk_anak'];
     
-    } else if($op == "update"){
-        $nama_anak = $_GET['nama_anak'];
-        $nik_anak = htmlspecialchars($_GET['nik_anak']);
-        $tempat_lahir_anak = htmlspecialchars($_GET['tempat_lahir_anak']);
+    } else if($func_anak == "update_data_anak"){
+        $nik_anak = $_GET['option_nik_anak'];
+        $nama_anak = htmlspecialchars($_GET['nama_anak']);
+        $tempat_lahir_anak = htmlspecialchars($_GET['temp_lahir_anak']);
         $tgl_lahir_anak = htmlspecialchars($_GET['tgl_lahir_anak']);
-        $usia = htmlspecialchars($_GET['usia_anak']);
+        $usia_anak = htmlspecialchars($_GET['usia_anak']);
         $jk_anak = htmlspecialchars($_GET['jk_anak']);
-        $update = mysqli_query($conn, "UPDATE ref_anak SET nik_anak='$nik_anak', tempat_lahir_anak='$tempat_lahir_anak', tanggal_lahir_anak='$tgl_lahir_anak', usia='$usia', jk_anak='$jk_anak' WHERE nama_anak='$nama_anak'");
+        $update = mysqli_query($conn, "UPDATE ref_anak SET nama_anak='$nama_anak', tempat_lahir_anak='$tempat_lahir_anak', tanggal_lahir_anak='$tgl_lahir_anak', usia='$usia_anak', jk_anak='$jk_anak' WHERE nik_anak='$nik_anak'");
         
         if($update){
             echo "sukses";
@@ -32,24 +32,24 @@
             echo "error";
         }
    
-    } else if($op == "delete"){
-        $nama_anak = $_GET['nama_anak'];
-        $del = mysqli_query($conn, "DELETE FROM ref_anak WHERE nama_anak='$nama_anak'");
+    } else if($func_anak == "del_data_anak"){
+        $nik_anak = $_GET['option_nik_anak'];
+        $del = mysqli_query($conn, "DELETE FROM ref_anak WHERE nik_anak='$nik_anak'");
         
         if($del){
             echo "sukses";
         } else {
             echo "error";
         }
-   } else if($op == "tambah"){   
-        $nama_anak2 = $_GET['nama_anak2'];
-        $nik_anak = htmlspecialchars($_GET['nik_anak']);
-        $tempat_lahir_anak = htmlspecialchars($_GET['tempat_lahir_anak']);
+   } else if($func_anak == "tambah_data_anak"){   
+        $nik_anak = $_GET['nik_anak'];
+        $nama_anak = htmlspecialchars($_GET['nama_anak']);
+        $tempat_lahir_anak = htmlspecialchars($_GET['temp_lahir_anak']);
         $tgl_lahir_anak = htmlspecialchars($_GET['tgl_lahir_anak']);
         $usia_anak = htmlspecialchars($_GET['usia_anak']);
         $jk_anak = htmlspecialchars($_GET['jk_anak']);  
         $tambah = mysqli_query($conn, "INSERT INTO ref_anak(id_anak, nama_anak, nik_anak, tempat_lahir_anak, tanggal_lahir_anak, usia, jk_anak) 
-                                    VALUES('', '$nama_anak2','$nik_anak','$tempat_lahir_anak','$tgl_lahir_anak', '$usia_anak', '$jk_anak')");
+                                    VALUES('', '$nama_anak','$nik_anak','$tempat_lahir_anak','$tgl_lahir_anak', '$usia_anak', '$jk_anak')");
         
         if($tambah){
             echo "sukses";
--- prosesCrudIbu.php
+++ prosesCrudIbu.php
@@ -1,28 +1,28 @@
 <?php
     include 'connection.php';
 
-    $op = $_GET['op'];
+    $func_ibu = $_GET['func_ibu'];
 
-    if($op == "ambiloption"){
-        $option = mysqli_query($conn, "SELECT nama_ibu FROM ref_ibu");
+    if($func_ibu == "ambil_option_ibu"){
+        $option_ibu = mysqli_query($conn, "SELECT nik_ibu FROM ref_ibu");
         echo "<option>Pilih Nama Ibu</option>\n";
-        while($op = mysqli_fetch_array($option)){
-            echo "<option>".$op['nama_ibu']."</option>\n";
+        while($func_ibu = mysqli_fetch_array($option_ibu)){
+            echo "<option>".$func_ibu['nik_ibu']."</option>\n";
         }
     
-    } else if($op == "ambildata"){
-        $nama_ibu = $_GET['nama_ibu'];
-        $data = mysqli_query($conn, "SELECT * FROM ref_ibu WHERE nama_ibu='$nama_ibu'");
-        $d = mysqli_fetch_array($data);
-        echo $d['nik_ibu']."|".$d['alamat_ibu']."|".$d['no_telp_ibu'];
+    } else if($func_ibu == "ambil_data_ibu"){
+        $nik_ibu = $_GET['option_nik_ibu'];
+        $data_ibu = mysqli_query($conn, "SELECT * FROM ref_ibu WHERE nik_ibu='$nik_ibu'");
+        $d = mysqli_fetch_array($data_ibu);
+        echo $d['nama_ibu']."|".$d['alamat_ibu']."|".$d['telp_ibu'];
     
-    } else if($op == "update"){
-        $nama_ibu = $_GET['nama_ibu'];
-        $nik_ibu = htmlspecialchars($_GET['nik_ibu']);
+    } else if($func_ibu == "update_data_ibu"){
+        $nik_ibu = $_GET['option_nik_ibu'];
+        $nama_ibu = htmlspecialchars($_GET['nama_ibu']);
         $alamat_ibu = htmlspecialchars($_GET['alamat_ibu']);
-        $no_telp_ibu = htmlspecialchars($_GET['no_telp_ibu']);
-        $update = mysqli_query($conn, "UPDATE ref_ibu SET nik_ibu='$nik_ibu', alamat_ibu='$alamat_ibu', 
-                                            no_telp_ibu='$no_telp_ibu' WHERE nama_ibu='$nama_ibu'");
+        $telp_ibu = htmlspecialchars($_GET['telp_ibu']);
+        $update = mysqli_query($conn, "UPDATE ref_ibu SET nama_ibu='$nama_ibu', alamat_ibu='$alamat_ibu', 
+                                            telp_ibu='$telp_ibu' WHERE nik_ibu='$nik_ibu'");
         
         if($update){
             echo "sukses";
@@ -30,22 +30,22 @@
             echo "error";
         }
    
-    } else if($op == "delete"){
-        $nama_ibu = $_GET['nama_ibu'];
-        $del = mysqli_query($conn, "DELETE FROM ref_ibu WHERE nama_ibu='$nama_ibu'");
+    } else if($func_ibu == "del_data_ibu"){
+        $nik_ibu = $_GET['option_nik_ibu'];
+        $del = mysqli_query($conn, "DELETE FROM ref_ibu WHERE nik_ibu='$nik_ibu'");
         
         if($del){
             echo "sukses";
         } else {
             echo "error";
         }
-   } else if($op == "tambah"){   
-        $nama_ibu2 = $_GET['nama_ibu2'];
-        $nik_ibu = htmlspecialchars($_GET['nik_ibu']);
+   } else if($func_ibu == "tambah_data_ibu"){   
+        $nik_ibu = $_GET['nik_ibu'];
+        $nama_ibu = htmlspecialchars($_GET['nama_ibu']);
         $alamat_ibu = htmlspecialchars($_GET['alamat_ibu']);
-        $no_telp_ibu = htmlspecialchars($_GET['no_telp_ibu']);
-        $tambah = mysqli_query($conn, "INSERT INTO ref_ibu(id_ibu, nama_ibu, nik_ibu, alamat_ibu, no_telp_ibu) 
-                                        VALUES('', '$nama_ibu2','$nik_ibu','$alamat_ibu','$no_telp_ibu')");
+        $telp_ibu = htmlspecialchars($_GET['telp_ibu']);
+        $tambah = mysqli_query($conn, "INSERT INTO ref_ibu(id_ibu, nama_ibu, nik_ibu, alamat_ibu, telp_ibu) 
+                                        VALUES('', '$nama_ibu','$nik_ibu','$alamat_ibu','$telp_ibu')");
         
         if($tambah){
             echo "sukses";
