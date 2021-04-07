d00187b2cc25c3e41981cc0cbf481f1365e99d80
Meilio Andree <meilioAndree27@gmail.com>
2021-04-07 16:13:12 +0700

    Adding CRUD Imunisasi

--- prosesCrudImunisasi.php
+++ prosesCrudImunisasi.php
@@ -8,7 +8,7 @@
         while ($d = mysqli_fetch_array($query)){
             echo $d['id_imunisasi']."|".$d['tgl_imunisasi']."|".$d['usia_saat_vaksin']. "|". $d['tinggi_badan']. "|". $d['berat_badan']. "|". $d['periode'];
         }
-    }
+    //}
 //      else if($op == "update"){
 //         $tgl_imunisasi = $_GET['tgl_imunisasi'];
 //         $usia_saat_vaksin = htmlspecialchars($_GET['usia_saat_vaksin']);
@@ -30,29 +30,31 @@
 //             echo "error";
 //         }
    
-//     } else if($op == "delete"){
-//         $tgl_imunisasi = $_GET['tgl_imunisasi'];
-//         $del = mysqli_query($conn, "DELETE FROM ref_imunisasi WHERE tgl_imunisasi='$tgl_imunisasi'");
+    } else if($op == "delete"){
+        $id_imun = $_GET['id_imun'];
+        $del = mysqli_query($conn, "DELETE FROM ref_imunisasi WHERE id_imun='$id_imun'");
         
-//         if($del){
-//             echo "sukses";
-//         } else {
-//             echo "error";
-//         }
-//    } else if($op == "tambah"){   
-//         $tgl_imunisasi2 = $_GET['tgl_imunisasi2'];
-//         $usia_saat_vaksin = htmlspecialchars($_GET['usia_saat_vaksin']);
-//         $tinggi_badan = htmlspecialchars($_GET['tinggi_badan']);
-//         $berat_badan = $_GET['berat$berat_badan'];
-//         $periode = htmlspecialchars($_GET['periode']);
+        if($del){
+            echo "sukses";
+        } else {
+            echo "error";
+        }
 
-//         $tambah = mysqli_query($conn, "INSERT INTO ref_imunisasi(id_imunisasi, tgl_imunisasi, usia_saat_vaksin, tinggi_badan, berat_badan, periode) 
-//                         VALUES('', '$tgl_imunisasi2','$usia_saat_vaksin','$tinggi_badan','$berat_badan', '$periode')");
+   } else if($func_imun == "tambah_data_imun"){   
+        $tgl_imunisasi = $_GET['tgl_imun'];
+        $usia_saat_vaksin = htmlspecialchars($_GET['usia_saat_vaksin']);
+        $tinggi_badan = htmlspecialchars($_GET['tinggi_badan']);
+        $berat_badan = htmlspecialchars($_GET['berat_badan']);
+        $periode = htmlspecialchars($_GET['periode']);
+
+        $tambah = mysqli_query($conn, "INSERT INTO ref_imunisasi(id_imunisasi, tgl_imunisasi, usia_saat_vaksin, tinggi_badan, berat_badan, periode) 
+                        VALUES('', '$tgl_imunisasi','$usia_saat_vaksin','$tinggi_badan','$berat_badan', '$periode')");
         
-//         if($tambah){
-//             echo "sukses";
-//         } else {
-//             echo "ERROR";
-//         }
+        if($tambah){
+            echo "sukses";
+        } else {
+            echo "ERROR";
+        }
+   }
 //     }
 ?>
